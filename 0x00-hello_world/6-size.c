e58702b441dc702c42ade23fa8dc8b3a236427a1
Femtosh <michael.omotoso5@gmail.com>
2022-09-08 23:59:35 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,9 +15,9 @@ int main(void)
 
 	printf("Size of a char: %Id byte(s)\n", sizeof(c));
 	printf("Size of an int: %Id byte(s)\n", sizeof(i));
-	printf("Size of a long int: byte(s)%Id \n", sizeof(li));
-	printf("Size of a long long int: byte(s)%Id \n", sizeof(lli));
-	printf("Size of a float: byte(s)%Id \n", sizeof(f));
+	printf("Size of a long int: %Id byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %Id byte(s)\n", sizeof(lli));
+	printf("Size of a float: %Id byte(s)\n", sizeof(f));
 
 	return (0);
 }
