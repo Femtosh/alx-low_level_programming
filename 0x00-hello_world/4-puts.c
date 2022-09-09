5c78f6b75538598f9640d0985638a903dd9fce02
Femtosh <michael.omotoso5@gmail.com>
2022-09-08 22:41:44 -0700

    new

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * comment starts here
- * This programme prints Programming is like building a multilingual puzzle
- * Retunn 0;
- *
- */
+* comment starts here
+* This programme prints Programming is like building a multilingual puzzle
+* Retunn 0;
+*
+*/
 
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
