1edd5c0bcde1354019f03df1ce9f259681620db6
Femtosh <michael.omotoso5@gmail.com>
2022-09-13 07:59:33 -0700

    comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int b;
+	
+	for (b = 48; b <= 57, b++)
+	{
+		putchar(c);
+		if (b != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
