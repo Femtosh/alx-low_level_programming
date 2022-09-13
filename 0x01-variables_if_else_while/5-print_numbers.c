f333e07632427aa54a4680e90368293b637e5a7b
Femtosh <michael.omotoso5@gmail.com>
2022-09-13 06:20:11 -0700

    numb

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int l;
+
+	for (l = 0; l < 10; l++)
+	{
+		printf("%i", l);
+	}
+	printf("\n");
+	return (0);
+}
