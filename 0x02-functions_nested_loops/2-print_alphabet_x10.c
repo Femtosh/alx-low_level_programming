bc09fb3a445c6bb09ebd6ebab98ca15f74dbffc0
Femtosh <michael.omotoso5@gmail.com>
2022-09-14 14:27:37 -0700

    az

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main"
+/**
+ * print_alphabet_x10 - function prints a-z ten times
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char c, i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
