6735db4fa02ac1581089e33e775fcf8baf425405
Femtosh <michael.omotoso5@gmail.com>
2022-09-15 19:44:53 -0700

    large

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 void positive_or_negative(int i);
-int largest number(int, int, int);
+int largest_number(int, int, int);
 void print_remaining_days(int, int, int);
 int convert_day(int, int);
 
