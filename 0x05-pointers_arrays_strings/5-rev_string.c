c2902b0cd6863e8b2c2f14ce4096bcec8247bd27
Femtosh <michael.omotoso5@gmail.com>
2022-09-20 05:30:33 -0700

    rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - reverses string
+ * @s: String to reverse
+ * return: Always 0 (Success)
+ */
+void rev_string(char *s)
+{
+	int len = strlen(s);
+
+	while (len--)
+		putchar(*(s + len));
+	putchar(10);
+}
