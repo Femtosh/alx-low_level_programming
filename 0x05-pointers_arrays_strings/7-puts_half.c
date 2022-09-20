de874cea77420c5a4177fd5483b379e87135739a
Femtosh <michael.omotoso5@gmail.com>
2022-09-20 09:36:55 -0700

    r

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,24 +10,21 @@
 void puts_half(char *str)
 {
 	int n;
+	int i;
 	int length_of_the_string = strlen(str);
 
+	i = (length_of_the_string / 2);
+	n = ((length_of_the_string - 1) / 2);
 	while (*(str + n) != '\0')
 	{
 	if (length_of_the_string % 2 == 0)
 	{
-	for (n = length_of_the_string / 2)
-		{
-			putchar(*(str + n));
-			n++;
+		putchar(*(str + i));
+			i++;
 		}
-	}
 	else {
-	for (n = (length_of_the_string - 1) / 2)
-	{
-	putchar (*(str + n));
-	n++;
-	}
+		putchar (*(str + n));
+		n++;
 	}
 	}
 }
