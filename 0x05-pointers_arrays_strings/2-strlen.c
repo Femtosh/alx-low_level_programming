bdd8aeda6b57e5869843e77b3cf9582d9f3613ba
Femtosh <michael.omotoso5@gmail.com>
2022-09-20 03:48:31 -0700

    lenght

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strlen - gives the length of a string
+ * @s: String decleard
+ * Return: Length of a string
+ */
+
+int_strlen(char *s)
+{
+	int len = 0;
+	while (*(s + len) != '\0')
+		len++;
+
+	return (len);
+	
+}
