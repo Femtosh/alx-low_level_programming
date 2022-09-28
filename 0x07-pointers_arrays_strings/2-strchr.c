72c5f135d2ec813bb5a8351a5ef10039b568d462
Femtosh <michael.omotoso5@gmail.com>
2022-09-27 19:12:17 -0700

    chack

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * strchr - checks character in a string
+ * @s: strings to check
+ * @c: character to check
+ *
+ * Return - Always null
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; *(s + i); i++)
+	{
+		if(*(s + i) == c)
+		return (s + i);
+	}
+	if (*s(s + i) == c)
+		return (s + i);
+	return (0);
+}
+
