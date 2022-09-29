5ef9fa5ae0e6b1b46856642d9606c9eb5fa313db
Femtosh <michael.omotoso5@gmail.com>
2022-09-29 06:43:32 -0700

    fact

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * factorial - function finds factorial
+ * @n: number to find its factorial
+ *
+ * Return: Always 0 (Success)
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	else
+		return (n * factorial(n-1));
+}
