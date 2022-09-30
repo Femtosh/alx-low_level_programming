6a2d2f943c1b9f9cfe0d29cf7978e7a760f4adfd
Femtosh <michael.omotoso5@gmail.com>
2022-09-30 08:55:18 -0700

    r

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if a charcater is digit
+ * @c: Character
+ *
+ * Return: Always 0 (Success)
+ */
+int _isdigit(int c)
+{
+}
---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _atoi - Checks the char s
+ * @s: Character s
+ *
+ * Return: Always 0 (Success)
+ */
+int _atoi(char *s)
+{
+}
