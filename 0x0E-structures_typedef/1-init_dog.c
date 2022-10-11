ffe7df17086f84496492c1ccfff39c14dbdc097d
Femtosh <michael.omotoso5@gmail.com>
2022-10-11 09:59:41 -0700

    dog info

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 
 /**
@@ -9,15 +9,12 @@
  * @owner: owner
  * Descritpion: This struct is for dog
  */
-
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
 		return;
-	else
-	{
+
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
-	}
 }
