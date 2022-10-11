c83c4af46156f590b6f38856a4d4cda9b4360364
Femtosh <michael.omotoso5@gmail.com>
2022-10-11 13:53:25 -0700

    dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,17 +2,39 @@
 #include "dog.h"
 
 /**
- * free_dog - free memory
- * @d: array
+ * new_dog - creates a new dog
+ * @name: Name
+ * @age: Age
+ * @owner: Owner
  * Desription: free memory for struct
  */
 
-void free_dog(dog_t *d)
+dog_t *new_dog(char *name, float age, char *owner);
 {
-	if (d)
+	dog_t *doggs;
+
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
+
+	doggs = malloc(sizeof(dog_t));
+	if (doggs == NULL)
+		return (NULL);
+	doggs->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (doggs->name == NULL)
 	{
-		free(d->name);
-		free(d->woner);
-		free(d);
+		free(doggs);
+		return (NULL);
 	}
+	doggs->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (doggs->owner == NULL)
+	{
+		free(doggs->name);
+		free(doggs);
+		return (NULL);
+	}
+	doggs->name = _strcopy(doggs->name, name);
+	doggs->age = age;
+	doggs->owner = _strcopy(doggs->owner, owner);
+
+	return (doggs);
 }
