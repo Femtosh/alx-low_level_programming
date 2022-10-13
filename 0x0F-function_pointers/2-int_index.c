63a1ce2d008f36ada3ff859cd10ef9fa075861cf
Femtosh <michael.omotoso5@gmail.com>
2022-10-13 15:11:33 -0700

    array

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - finds and integer
+ * array: array of intergers
+ * @size: size of the array
+ * @cmp : the pointer
+ *
+ * Return: Always 0 (success)
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, res;
+
+	res = -1;
+	if (array && cmp)
+	{
+		if (size <= 0)
+		{
+			return (res);
+		}
+		for (i = 0; i < size; i++)
+		{
+			cmp(array[i]);
+			if (cmp(array[i]) > 0)
+				{
+					res = 1;
+					break;
+				}
+				if ((cmp(array[i]) == (-1)))
+				{
+					return (res);
+				}
+		}
+	}
+	return (res);
+}
+
+				
